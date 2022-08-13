9f3ea5681e2ce8b500447c1e909ad30eadc6cd65
Jeongbin <yon980824@gmail.com>
2022-08-13 11:13:22 +0900

    Project setup

---
+++ src/theme.ts
@@ -0,0 +1,9 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "#2f3640",
+  textColor: "#808e9b",
+  accentColor: "#8c7ae6",
+  viewColor: "#1e272e",
+  tabColor: "#1e272e",
+};
